144deda4ad79c6ae0499f2e273d31367d32b953e
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-02-26 02:51:04 -0300

    ADD: status code and auth messages response

---
+++ src/constants/auth.messages.js
@@ -0,0 +1,11 @@
+const CREATED_USER =  "Successfully created an user.";
+const ALL_THE_USERS = "Fetched the user(s).";
+const ERROR_LOGIN = "User or password wrong.";
+const INVALID_TOKEN = "Invalid token.";
+
+module.exports = {
+    CREATED_USER,
+    ALL_THE_USERS,
+    ERROR_LOGIN,
+    INVALID_TOKEN
+};
