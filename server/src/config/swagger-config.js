6f732262e84d2d7fc38835a3c7fe6aeb6ea7994e
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-02-27 23:20:52 -0300

    ADD: swagger config

---
+++ server/src/config/swagger-config.js
@@ -0,0 +1,35 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+require('dotenv').config();
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'API-venta de productos',
+        description: 'Este proyecto sirve de base para las operaciones diarias en un negocio pequeño',
+        version: '1.0.0',
+    },
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: process.env.SWAGGER_SECURITY_TYPE,
+                scheme: process.env.SECURITY_SCHEMA,
+                bearerFormat: process.env.BEARER_FORMAT,
+            },
+        },
+    },
+    servers: [
+        {
+            url: `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`,
+            description: 'API server',
+        },
+    ],
+};
+
+const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes/*.js'],
+};
+
+const swaggetSpect = swaggerJsdoc(options);
+module.exports = swaggetSpect;
